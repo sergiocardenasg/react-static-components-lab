cca20df726292d76379c31158f30bbdc62283a00
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-13 20:05:05 -0500

    Revert "solved"
    This reverts commit ea7e9063dc47c4327220d4a3dd3ae4e600899218.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import CatComponent from './CatComponent.js'
-import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
-import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
@@ -10,7 +7,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				<MouseComponent />
+				{/* one more component missing */}
 			</div>
 		);
 	}
